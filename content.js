73e1fbfb40cbdd923391f91a6f28636c913199ad
ituyama <42466955+ituyama@users.noreply.github.com>
2020-09-13 17:38:27 +0900

    aaaa
    aaaa

--- content.js
+++ content.js
@@ -5,25 +5,30 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function kana() {
+
+
+	const url = ;
+
+	    $.getJSON("https://ituyama.github.io/kana/kana.json?callback=?", (data) => {
+	      for (let i=0; i<data.length; i++){
+	        console.log(`userid=${data[i].kana_list}, username=${data[i].kanji_list}`);
+
+
+
   var body = $("body").html();
-
-  var result = body.replace(/テクノロジー/g,"<strong>技術</strong>")
-                  .replace(/ビジョン/g,"<strong>計画</strong>")
-                  .replace(/テック/g,"<strong>技術</strong>")
-                  .replace(/クラッシュ/g,"<strong>崩壊</strong>")
-                  .replace(/トレンド/g,"<strong>流行</strong>")
-                  .replace(/サプライ/g,"<strong>流通</strong>")
-                  .replace(/キャリア/g,"<strong>仕事</strong>")
-                  .replace(/インパクト/g,"<strong>衝撃</strong>")
-                  .replace(/イノベーション/g,"<strong>革新</strong>")
-                  .replace(/ユーザ/g,"<strong>利用者</strong>")
-                  .replace(/エクスペリエンス/g,"<strong>体験</strong>")
-                  .replace(/レポート/g,"<strong>報告</strong>")
-                  .replace(/データ/g,"<strong>情報</strong>")
-                  .replace(/アクション/g,"<strong>行動</strong>")
-
 
 
 
   $("body").html(result);
+
+	      }
+	    });
+
+
+
+
+
+
+
+
 }
